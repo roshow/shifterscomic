8916d8dfce0747d137c8b6457687bac3f34bbfe2
roshow <roshow@gmail.com>
2017-07-02 21:29:18 -0400

    moved connected components to containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,37 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom'
 import Comic from './Comic'
-import Home from './../components/Home'
-import Nav from './../components/Nav'
+import Home from './Home'
+import Nav from './Nav'
 import Header from './Header'
 
 import './App.css';
 
 
-class App extends Component {
-
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      chapters: {}
-    }
-
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/chapter/:chapterId" component={ Comic } />
-          <Route component={ Home } />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div className="App">
+    <Header />
+    <Nav />
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/chapter/:chapterId" component={ Comic } />
+      <Route component={ Home } />
+    </Switch>
+  </div>
+)
 
 export default App;
